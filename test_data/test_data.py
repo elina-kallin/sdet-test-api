4446a5887c78b728e42568656f5e79de752bbf68
Елена Бакальская <elina_kallin@mail.ru>
2026-03-24 01:08:51 +0400

    добавила тестовые данные из тест-кейса

---
+++ test_data/test_data.py
@@ -0,0 +1,25 @@
+POST_CREATE_DATA = {
+    "title": "Тестовый пост API",
+    "content": "Это содержимое тестового поста",
+    "status": "publish",
+    "author": 1,
+    "comment_status": "open",
+}
+
+POST_UPDATE_DATA = {
+    "title": "Тестовый пост API 2",
+    "content": "Это содержимое тестового поста",
+    "status": "publish",
+    "author": 1,
+    "comment_status": "open",
+}
+
+COMMENT_CREATE_DATA = {
+    "content": "комментарий к записи для тестирования",
+    "author": 1,
+}
+
+COMMENT_UPDATE_DATA = {
+    "content": "комментарий к записи для тестирования UPDATE",
+    "author": 1,
+}
