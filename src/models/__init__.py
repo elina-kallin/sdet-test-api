fcb723ba3266c688e0bdf55d5f1c3edbd44ab89d
Елена Бакальская <elina_kallin@mail.ru>
2026-03-23 23:25:53 +0400

    по аналогии сделала модели pydantic для комментариев

---
+++ src/models/__init__.py
@@ -0,0 +1,16 @@
+from .post import (
+    PostBase,
+    PostCreate,
+    PostUpdate,
+    PostResponse,
+    PostStatus,
+    CommentStatus,
+)
+
+from .comment import (
+    CommentBase,
+    CommentCreate,
+    CommentUpdate,
+    CommentResponse,
+    CommentStatus,
+)
